38eab34628f99e10a571c53ffc68fea796649f9e
Bron Davies <bron.davies@aa.com>
2017-09-28 09:15:50 -0500

    Bug fixes and performance improvements

--- src/ghe.api.js
+++ src/ghe.api.js
@@ -1,23 +1,34 @@
-var ghe = {
-  usernames: {},
-  api: {
-    getUsername: (userid, after) => {
-      if (ghe.usernames[userid]) {
-        after(ghe.usernames[userid]);
-        return;
-      }
-      fetch('/' + userid, {
-        credentials: 'same-origin'
-      }).then(response => {
-        return response.text();
-      }).then(text => {
-        var matches = text.match('(.*)<title>' + userid + ' \\((.+)\\)<\\/title>');
-        if (matches && matches.length > 2) {
-          var name = matches[2];
-          ghe.usernames[userid] = name;
-          after(name);
-        }
-      })
-    }
+(function() {
+  var cacheTimer = 0;
+  function cache() {
+    clearTimeout(cacheTimer);
+    cacheTimer = setTimeout(cacheAsync, 1000);
   }
-};
+  function cacheAsync() {
+    localStorage.setItem('usernameCache', JSON.stringify(ghe.usernames));
+  }
+  window.ghe = {
+    usernames: JSON.parse(localStorage.getItem('usernameCache') || '{}'),
+    api: {
+      getUsername: (userid, after) => {
+        if (ghe.usernames[userid]) {
+          after(userid, ghe.usernames[userid]);
+          return;
+        }
+        fetch('/' + userid, {
+          credentials: 'same-origin'
+        }).then(response => {
+          return response.text();
+        }).then(text => {
+          var matches = text.match('(.*)<title>' + userid + ' \\((.+)\\)<\\/title>');
+          if (matches && matches.length > 2) {
+            var name = matches[2];
+            ghe.usernames[userid] = name;
+            after(userid, name);
+            cache();
+          }
+        })
+      }
+    }
+  };
+})();
--- src/ghe.js
+++ src/ghe.js
@@ -1,24 +1,40 @@
 (function() {
+  var ariaLabel = 'aria-label';
   function lookupUsernames() {
-    var elements = document.querySelectorAll('a.author,a.assignee,a.user-mention,a span.discussion-item-entity');
+    var elements = document.querySelectorAll(
+      'a.author' +
+      ',a.assignee' +
+      ',a.user-mention' +
+      ',a span.discussion-item-entity' +
+      ',.sidebar-assignee span p span.css-truncate-target.text-bold' +
+      ',a.participant-avatar.tooltipped'
+    );
     var usermap = {};
     elements.forEach (element => {
-      if (element && element.innerText) {
-        var userid = element.innerText.replace('@', '');
-        if (userid.match(/[A-Z,0]{1}[0-9]{7,8}/g)) {
-          usermap[userid] = usermap[userid] || [];
-          usermap[userid].push(element);
-        }
+      var userid = null;
+      if (element && element.hasAttribute(ariaLabel)) {
+        userid = element.getAttribute(ariaLabel);
+      } else if (element && element.innerText) {
+        userid = element.innerText.replace('@', '');
+      }
+      if (userid && userid.match(/[A-Z,0]{1}[0-9]{7,8}/g)) {
+        usermap[userid] = usermap[userid] || [];
+        usermap[userid].push(element);
       }
     });
     for (var userid in usermap) {
-      ghe.api.getUsername(userid, (name) => {
-        usermap[userid].forEach(element => {
-          element.innerText = name.toLowerCase().replace(/\b\w/g, l => l.toUpperCase());
+      ghe.api.getUsername(userid, (uid, name) => {
+        usermap[uid].forEach(element => {
+          var capitalized = name.toLowerCase().replace(/\b\w/g, l => l.toUpperCase());
+          if (element.hasAttribute(ariaLabel)) {
+            element.setAttribute(ariaLabel, capitalized);
+          } else {
+            element.innerText = capitalized;
+          }
         });
       });
     }
   }
-  lookupUsernames();
+  setTimeout(lookupUsernames, 250);
   setInterval(lookupUsernames, 5000);
 })();
